c7ecc62ed20fd431438d780f830b6b46d4af03cb
wangziyue.28 <wangziyue.28@jd.com>
2025-09-26 14:27:20 +0800

    bugfix: fix VLM runtime error when enable tensor parallel.

--- xllm/core/runtime/vlm_engine.cpp
+++ xllm/core/runtime/vlm_engine.cpp
@@ -278,7 +278,7 @@ ForwardOutput VLMEngine::step(std::vector<Batch>& batches) {
   for (auto& worker : workers_) {
     // TODO to adapt multi stream parallel later
     BatchedForwardInputs batched_fwd_inputs;
-    batched_fwd_inputs.micro_inputs = {std::move(forward_inputs)};
+    batched_fwd_inputs.micro_inputs = {forward_inputs};
     futures.emplace_back(worker->step_async(batched_fwd_inputs));
   }
   // wait for the all future to complete
