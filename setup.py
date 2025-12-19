1a200b697ee57600c50a19e3eab6005ef8e1c15b
zhangxu.709 <zhangxu.709@jd.com>
2025-12-19 19:53:56 +0800

    feat: add Dockerfile for cuda device.

--- setup.py
+++ setup.py
@@ -376,15 +376,17 @@ class ExtBuild(build_ext):
             cmake_args += ["-DUSE_MLU=ON"]
             set_mlu_envs()
         elif self.device == "cuda":
-            cuda_architectures = "80;89;90"
+            torch_cuda_architectures = os.getenv("TORCH_CUDA_ARCH_LIST")
+            if not torch_cuda_architectures:
+                raise ValueError("Please set TORCH_CUDA_ARCH_LIST environment variable, e.g. export TORCH_CUDA_ARCH_LIST=\"8.0 8.9 9.0 10.0 12.0\"")
             cmake_args += ["-DUSE_CUDA=ON", 
-                           f"-DCMAKE_CUDA_ARCHITECTURES={cuda_architectures}"]
+                           f"-DTORCH_CUDA_ARCH_LIST={torch_cuda_architectures}"]
             set_cuda_envs()
         elif self.device == "ilu":
             cmake_args += ["-DUSE_ILU=ON"]
             set_ilu_envs()
         else:
-            raise ValueError("Please set --device to a2 or a3 or mlu or cuda.")
+            raise ValueError("Please set --device to a2 or a3 or mlu or cuda or ilu.")
 
         product = "xllm"
         if self.generate_so:
