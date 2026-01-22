2e3d96c7a12cdb072d5366a8773f4a526ec8d9fd
K <22747336+kalashnlkov@users.noreply.github.com>
2026-01-22 20:01:57 +0800

    cicd: add cicd for iluvatar device. (#731)
    Co-authored-by: Dingxiao Chen <dingxiao.chen@iluvatar.com>

---
+++ cibuild/build_ilu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+function error() {
+  echo "Require build command, e.g. python setup.py build"
+  exit 1
+}
+
+REGISTRY="registry.iluvatar.com.cn:10443/infra"
+COREX_VERSION="4.4.0.20251023"
+IMAGE="${REGISTRY}/xllm-builder:${COREX_VERSION}-ubuntu22.04-py310-xllm-x86_64"
+
+RUN_OPTS=(
+  --rm
+  -t
+  --privileged
+  --ipc=host
+  --network=host
+  -v /export/home:/export/home
+  -v /export/home/ilu_vcpkg_cache:/root/.cache/vcpkg # cached vcpkg installed dir
+  -w /export/home
+)
+
+CMD="$*"
+[[ -z "${CMD}" ]] && error
+
+[[ ! -x $(command -v docker) ]] && echo "ERROR: 'docker' command is missing." && exit 1
+
+docker run "${RUN_OPTS[@]}" "${IMAGE}" bash -c "set -euo pipefail; cd $(pwd); ${CMD}"
